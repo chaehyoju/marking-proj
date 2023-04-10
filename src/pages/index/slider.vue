a5a037def6a2c3c56175445afb33e16597f236a2
Chae Hyo Ju <agleor@naver.com>
2023-04-10 14:07:30 +0900

    슬라이드 구현

--- src/pages/index/slider.vue
+++ src/pages/index/slider.vue
@@ -3,11 +3,22 @@
         최근 담화, 함께 볼까요?
      </div>
     <div class="content-center border w-96 rounded-xl m-auto h-36">
-        안녕
+        <swiper :modules="modules" :centeredSlides="true" :slides-per-view="auto" :loop="true" :autoplay="{delay: 5000,}" class="relative">
+            <swiper-slide><div class="text-center"><span>안녕</span></div></swiper-slide>
+            <swiper-slide><div class="text-center"><span>하이</span></div></swiper-slide>
+            <swiper-slide><div class="text-center"><span>롤린</span></div></swiper-slide>
+        </swiper>
+    </div>
+    <div class="m-auto w-40 mt-11">
+        <img src="@/assets/chat.png" style="width: 130px;">
     </div>
     
 </template>
 
 <script setup>
-// https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { Navigation, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+
+const modules = [Navigation, Autoplay];
 </script>
